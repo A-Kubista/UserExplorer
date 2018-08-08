c80a8aae25e8ae2eecc5cc5ad9bd2bf84ad6c697
A-Kubista <aleksanderkubista@gmail.com>
2018-08-08 18:59:25 +0200

    add subscription in viewmodel for getRepositories

--- app/src/main/java/com/kubista/repoexplorer/ui/repository/RepoListViewModel.kt
+++ app/src/main/java/com/kubista/repoexplorer/ui/repository/RepoListViewModel.kt
@@ -2,6 +2,9 @@ package com.kubista.repoexplorer.ui.repository
 
 import com.kubista.repoexplorer.base.BaseViewModel
 import com.kubista.repoexplorer.network.GitHubApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 /**
@@ -9,5 +12,39 @@ import javax.inject.Inject
  */
 class RepoListViewModel : BaseViewModel() {
     @Inject
-    lateinit var postApi: GitHubApi
+    lateinit var gitHubApi: GitHubApi
+
+    private lateinit var subscription: Disposable
+
+    init{
+        loadPosts()
+    }
+
+    private fun loadPosts(){
+        subscription = gitHubApi.getRepositories()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { onRetrievePostListStart() }
+                .doOnTerminate { onRetrievePostListFinish() }
+                .subscribe(
+                        { onRetrievePostListSuccess() },
+                        { onRetrievePostListError() }
+                )
+    }
+
+    private fun onRetrievePostListStart(){
+
+    }
+
+    private fun onRetrievePostListFinish(){
+
+    }
+
+    private fun onRetrievePostListSuccess(){
+
+    }
+
+    private fun onRetrievePostListError(){
+
+    }
 }
