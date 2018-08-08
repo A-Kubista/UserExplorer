435a72c7a7e7529a0d302b55a72b40d09d016e69
A-Kubista <aleksanderkubista@gmail.com>
2018-08-08 17:09:40 +0200

    add GitHub api placeholder
    with getRepos method placeholder

---
+++ app/src/main/java/com/kubista/repoexplorer/network/GitHubApi.kt
@@ -0,0 +1,20 @@
+package com.kubista.repoexplorer.network
+
+import com.kubista.repoexplorer.model.GithubRepo
+import retrofit2.http.GET
+import io.reactivex.Observable
+
+/**
+ * Created by alek on 08/08/2018.
+ */
+
+/**
+ * The interface which provides methods to get result of webservices
+ */
+interface GitHubApi {
+    /**
+     * Get the list of the pots from the API
+     */
+    @GET("/repositories")
+    fun getRepositories(): Observable<List<GithubRepo>>
+}
