4e8c28f0199a7eb204b85a0afac23e44494669aa
A-Kubista <aleksanderkubista@gmail.com>
2018-08-08 17:15:48 +0200

    add constants with github base url

---
+++ app/src/main/java/com/kubista/repoexplorer/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.kubista.repoexplorer.utils
+
+/**
+ * Created by alek on 08/08/2018.
+ */
+
+/** The base URL of the APIs */
+const val GITHUB_BASE_URL: String = "https://api.github.com/"
