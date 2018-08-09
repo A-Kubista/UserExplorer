1b4630990a39c5c7201118c80030d09d33e00934
A-Kubista <aleksanderkubista@gmail.com>
2018-08-09 09:42:25 +0200

    add binding adapters viewExtension nad  repo_list  activity layout

---
+++ app/src/main/java/com/kubista/repoexplorer/utils/BindingAdapters.kt
@@ -0,0 +1,20 @@
+package com.kubista.repoexplorer.utils
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.databinding.BindingAdapter
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.kubista.repoexplorer.utils.extension.getParentActivity
+
+/**
+ * Created by alek on 09/08/2018.
+ */
+
+@BindingAdapter("mutableVisibility")
+fun setMutableVisibility(view: View, visibility: MutableLiveData<Int>?) {
+    val parentActivity: AppCompatActivity? = view.getParentActivity()
+    if(parentActivity != null && visibility != null) {
+        visibility.observe(parentActivity, Observer { value -> view.visibility = value?:View.VISIBLE})
+    }
+}
