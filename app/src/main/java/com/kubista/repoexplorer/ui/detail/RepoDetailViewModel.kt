2ea01162986c22f64e4554a2263ab68675b1e573
A-Kubista <aleksanderkubista@gmail.com>
2018-08-10 01:24:27 +0200

    add description in detailed repo view, refacotr repoBody to repoDesc

--- app/src/main/java/com/kubista/repoexplorer/ui/detail/RepoDetailViewModel.kt
+++ app/src/main/java/com/kubista/repoexplorer/ui/detail/RepoDetailViewModel.kt
@@ -1,19 +1,17 @@
 package com.kubista.repoexplorer.ui.detail
 
-import android.arch.lifecycle.MutableLiveData
 import com.kubista.repoexplorer.BaseViewModel
-import com.kubista.repoexplorer.model.GitHubRepo
 import com.kubista.repoexplorer.model.IRepo
 
 class RepoDetailViewModel : BaseViewModel() {
     private lateinit var repoTitle : String
-    private lateinit var repoBody : String
+    private lateinit var repoDesc : String
     private lateinit var repoOwnerLogin : String
     private lateinit var repoOwnerAvatar : String
 
     fun bind(repo: IRepo) {
         repoTitle = repo.getRepositoryTitle()
-        repoBody = repo.getRepositoryDescription()
+        repoDesc = repo.getRepositoryDescription()
         repoOwnerLogin = repo.getOwnerName()
         repoOwnerAvatar = repo.getOwnerAvatarUrl()
     }
@@ -22,8 +20,8 @@ class RepoDetailViewModel : BaseViewModel() {
         return repoTitle
     }
 
-    fun getRepoBody(): String {
-        return repoBody
+    fun getRepoDesc(): String {
+        return repoDesc
     }
 
     fun getRepoOwnerLogin(): String {
--- app/src/main/java/com/kubista/repoexplorer/ui/repo/RepoViewModel.kt
+++ app/src/main/java/com/kubista/repoexplorer/ui/repo/RepoViewModel.kt
@@ -2,18 +2,17 @@ package com.kubista.repoexplorer.ui.repo
 
 import android.arch.lifecycle.MutableLiveData
 import com.kubista.repoexplorer.BaseViewModel
-import com.kubista.repoexplorer.model.GitHubRepo
 import com.kubista.repoexplorer.model.IRepo
 
 class RepoViewModel : BaseViewModel() {
     private val repoTitle = MutableLiveData<String>()
-    private val repoBody = MutableLiveData<String>()
+    private val repoDesc = MutableLiveData<String>()
     private val repoOwnerLogin = MutableLiveData<String>()
     private val repoOwnerAvatar = MutableLiveData<String>()
 
     fun bind(repo: IRepo) {
         repoTitle.value = repo.getRepositoryTitle()
-        repoBody.value = repo.getRepositoryDescription()
+        repoDesc.value = repo.getRepositoryDescription()
         repoOwnerLogin.value = repo.getOwnerName()
         repoOwnerAvatar.value = repo.getOwnerAvatarUrl()
     }
@@ -22,8 +21,8 @@ class RepoViewModel : BaseViewModel() {
         return repoTitle
     }
 
-    fun getRepoBody(): MutableLiveData<String> {
-        return repoBody
+    fun getRepoDesc(): MutableLiveData<String> {
+        return repoDesc
     }
 
     fun getRepoOwnerLogin(): MutableLiveData<String> {
