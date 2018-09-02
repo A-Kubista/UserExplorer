bd67a8947604f7e92665764459c942e35aa94237
A-Kubista <aleksanderkubista@gmail.com>
2018-09-02 15:41:14 +0200

    add doc for viemodel factory function

--- app/src/main/java/com/kubista/userexplorer/base/BaseViewModelFactory.kt
+++ app/src/main/java/com/kubista/userexplorer/base/BaseViewModelFactory.kt
@@ -3,6 +3,13 @@ package com.kubista.userexplorer.base
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 
+/**
+ *  custom viewModel factory to create instance of generic type
+ *  with custom constructor, as BaseViewModel is abstract it
+ *  is impossible to create and return its instance, nor instance
+ *  of generic type without reflection
+ */
+@Suppress("UNCHECKED_CAST")
 inline fun <VM : ViewModel> viewModelFactory(crossinline f: () -> VM) =
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(aClass: Class<T>):T = f() as T
