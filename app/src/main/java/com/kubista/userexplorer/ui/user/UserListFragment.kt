60359ccd4ead7ad7607c2477a97489590b85e691
A-Kubista <aleksanderkubista@gmail.com>
2018-09-04 23:15:41 +0200

    add custom saving recycle view state
    - add saving state to bundle

--- app/src/main/java/com/kubista/userexplorer/ui/user/UserListFragment.kt
+++ app/src/main/java/com/kubista/userexplorer/ui/user/UserListFragment.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Parcelable
 import android.support.annotation.StringRes
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
@@ -21,6 +22,7 @@ import com.kubista.userexplorer.injection.module.DatabaseModule
 import com.kubista.userexplorer.model.user.UserDao
 import com.kubista.userexplorer.model.user.UserRepository
 import com.kubista.userexplorer.ui.detail.UserDetailActivity
+import com.kubista.userexplorer.utils.KEY_LIST_STATE
 import com.kubista.userexplorer.utils.KEY_USER_PARCEL
 import javax.inject.Inject
 
@@ -58,6 +60,10 @@ class UserListFragment : Fragment() {
         errorSnackbar?.dismiss()
     }
 
+    /**
+     *  in general android mvvm handles saving view state,
+     *  loading recycleview state is only for task purpose only
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_user_list, container, false)
@@ -66,6 +72,9 @@ class UserListFragment : Fragment() {
 
         binding.viewModel = viewModel
         binding.userList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        val state = savedInstanceState?.getParcelable<Parcelable>(KEY_LIST_STATE)
+        if (state != null) (binding.userList.layoutManager as LinearLayoutManager).onRestoreInstanceState(state)
 
         viewModel.errorMessage.observe(this, Observer { errorMessage ->
             if (errorMessage != null) showError(errorMessage) else hideError()
@@ -78,5 +87,11 @@ class UserListFragment : Fragment() {
         })
 
         return myView
+    }
+
+    override fun onSaveInstanceState(state: Bundle) {
+        super.onSaveInstanceState(state)
+        val mListState = binding.userList.layoutManager?.onSaveInstanceState()
+        state.putParcelable(KEY_LIST_STATE, mListState)
     }
 }
--- app/src/main/java/com/kubista/userexplorer/utils/Constants.kt
+++ app/src/main/java/com/kubista/userexplorer/utils/Constants.kt
@@ -7,5 +7,6 @@ const val BASE_URL_DAILYMOTION: String = "https://api.dailymotion.com/"
 const val KEY_USER_PARCEL: String = "user_parcel"
 const val KEY_SAVED_USER_TYPE: String = "selected_user_type"
 const val KEY_SAVED_USER_ID: String = "selected_user_id"
+const val KEY_LIST_STATE: String = "list_state"
 const val KEY_ERROR_MESSAGE_PARCEL: String = "error_msg_parcel"
 const val NO_INT_VAL : Int = -1
